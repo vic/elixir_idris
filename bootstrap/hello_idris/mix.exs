0f29652053fd0d4f456c288a8e277e169558db42
Victor Borja <vborja@apache.org>
2018-03-12 17:14:36 -0600

    move hello_idris example app

--- bootstrap/hello_idris/mix.exs
+++ bootstrap/hello_idris/mix.exs
@@ -27,7 +27,7 @@ defmodule HelloIdris.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:idris_bootstrap_json, path: "../../"}
+      {:idris_bootstrap_json, path: "../idris_json"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
