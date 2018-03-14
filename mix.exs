71ec8a0a1add610219150ecc08203ae906736cf2
Victor Borja <vborja@apache.org>
2018-03-13 18:07:11 -0600

    Make umbrella

--- mix.exs
+++ mix.exs
@@ -3,19 +3,18 @@ defmodule Idris.MixProject do
 
   def project do
     [
-      app: :idris,
-      version: "0.1.0",
-      elixir: "~> 1.6",
-      compilers: [],
+      apps_path: "apps",
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    []
   end
 end
