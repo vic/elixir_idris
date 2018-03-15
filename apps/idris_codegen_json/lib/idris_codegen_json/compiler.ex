3b8c4044e508f8e0789a7f11c2e75bfd722343af
Victor Borja <vborja@apache.org>
2018-03-15 04:19:35 -0600

    raising compiler

--- apps/idris_codegen_json/lib/idris_codegen_json/compiler.ex
+++ apps/idris_codegen_json/lib/idris_codegen_json/compiler.ex
@@ -3,46 +3,45 @@ defmodule Idris.Codegen.JSON.Compiler do
 
   defmacro __using__(do: code) do
     quote do
-      import Kernel
       import unquote(__MODULE__)
       unquote(code)
     end
   end
 
   def cg_Module(env, _, _) do
-    env.module
+    raise "TODO"
   end
 
-  def cg_ATInt(env, _var1), do: env.module
-  def cg_LChInt(env, _var1), do: env.module
-  def cg_LEq(env, _var1), do: env.module
-  def cg_LMinus(env, _var1), do: env.module
-  def cg_LSExt(env, _var1, _var2), do: env.module
-  def cg_LSLt(env, _var1), do: env.module
-  def cg_LExternal(env, _var1), do: env.module
-  def cg_LIntStr(env, _var1), do: env.module
-  def cg_LStrCons(env), do: env.module
-  def cg_LStrConcat(env), do: env.module
-  def cg_LStrEq(env), do: env.module
-  def cg_LStrHead(env), do: env.module
-  def cg_LStrTail(env), do: env.module
-  def cg_LWriteStr(env), do: env.module
-  def cg_Loc(env, _var1), do: env.module
-  def cg_SApp(env, _var1, _var2, _var3), do: env.module
-  def cg_SChkCase(env, _var1, _var2), do: env.module
-  def cg_SCase(env, _var1, _var2, _var3), do: env.module
-  def cg_SCon(env, _var1, _var2, _var3, _var4), do: env.module
-  def cg_SConCase(env, _var1, _var2, _var3, _var4, _var5), do: env.module
-  def cg_SConst(env, _var1), do: env.module
-  def cg_SConstCase(env, _var1, _var2), do: env.module
-  def cg_SDefaultCase(env, _var1), do: env.module
-  def cg_SFun(env, _var1, _var2, _var3, _var4), do: env.module
-  def cg_SLet(env, _var1, _var2, _var3), do: env.module
-  def cg_SNothing(env), do: env.module
-  def cg_SOp(env, _var1, _var2), do: env.module
-  def cg_SV(env, _var1), do: env.module
-  def cg_char(env, _var1), do: env.module
-  def cg_int(env, _var1), do: env.module
-  def cg_bigint(env, _var1), do: env.module
-  def cg_string(env, _var1), do: env.module
+  def cg_ATInt(env, _var1), do: raise "TODO"
+  def cg_LChInt(env, _var1), do: raise "TODO"
+  def cg_LEq(env, _var1), do: raise "TODO"
+  def cg_LMinus(env, _var1), do: raise "TODO"
+  def cg_LSExt(env, _var1, _var2), do: raise "TODO"
+  def cg_LSLt(env, _var1), do: raise "TODO"
+  def cg_LExternal(env, _var1), do: raise "TODO"
+  def cg_LIntStr(env, _var1), do: raise "TODO"
+  def cg_LStrCons(env), do: raise "TODO"
+  def cg_LStrConcat(env), do: raise "TODO"
+  def cg_LStrEq(env), do: raise "TODO"
+  def cg_LStrHead(env), do: raise "TODO"
+  def cg_LStrTail(env), do: raise "TODO"
+  def cg_LWriteStr(env), do: raise "TODO"
+  def cg_Loc(env, _var1), do: raise "TODO"
+  def cg_SApp(env, _var1, _var2, _var3), do: raise "TODO"
+  def cg_SChkCase(env, _var1, _var2), do: raise "TODO"
+  def cg_SCase(env, _var1, _var2, _var3), do: raise "TODO"
+  def cg_SCon(env, _var1, _var2, _var3, _var4), do: raise "TODO"
+  def cg_SConCase(env, _var1, _var2, _var3, _var4, _var5), do: raise "TODO"
+  def cg_SConst(env, _var1), do: raise "TODO"
+  def cg_SConstCase(env, _var1, _var2), do: raise "TODO"
+  def cg_SDefaultCase(env, _var1), do: raise "TODO"
+  def cg_SFun(env, _var1, _var2, _var3, _var4), do: raise "TODO"
+  def cg_SLet(env, _var1, _var2, _var3), do: raise "TODO"
+  def cg_SNothing(env), do: raise "TODO"
+  def cg_SOp(env, _var1, _var2), do: raise "TODO"
+  def cg_SV(env, _var1), do: raise "TODO"
+  def cg_char(env, _var1), do: raise "TODO"
+  def cg_int(env, _var1), do: raise "TODO"
+  def cg_bigint(env, _var1), do: raise "TODO"
+  def cg_string(env, _var1), do: raise "TODO"
 end
