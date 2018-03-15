836a4a1193bf441c7dae9fabbb87710ba9bcf183
Victor Borja <vborja@apache.org>
2018-03-15 13:44:09 -0600

    mod_fun_name

--- apps/idris_codegen_json/lib/idris_codegen_json/compiler.ex
+++ apps/idris_codegen_json/lib/idris_codegen_json/compiler.ex
@@ -1,14 +1,13 @@
 defmodule Idris.Codegen.JSON.Compiler do
-  @moduledoc false && """
+  @moduledoc false
 
-  https://github.com/idris-lang/Idris-dev/blob/f92ecd25d48a9120abfbb7d6af81f9b194ab98f1/src/IRTS/Portable.hs
-  """
+  import Idris.Codegen.JSON.Main, only: [mod_fun_name: 1]
 
   defmacro __using__(do: code) do
     implemented = __MODULE__.__info__(:functions)
+
     quote do
-      import unquote(__MODULE__.TODO),
-        except: unquote(implemented)
+      import unquote(__MODULE__.TODO), except: unquote(implemented)
       import unquote(__MODULE__)
       unquote(code)
     end
@@ -22,4 +21,13 @@ defmodule Idris.Codegen.JSON.Compiler do
     {:"var#{i}", [var: i, loc: i, fname: fname], module}
   end
 
+  def cg_SV(_ctx, lv), do: lv
+
+  def cg_SCon(_ctx, lv, _i, name, _vars) do
+    raise(inspect(mod_fun_name(name)))
+  end
+
+  def cg_SFun(_ctx, name, _args, _i, _exp) do
+    raise(inspect(mod_fun_name(name)))
+  end
 end
